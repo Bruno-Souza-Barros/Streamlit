bb55679c4b4b44896303e0653da363630acc75cb
Bruno-Souza-Barros <56643601+Bruno-Souza-Barros@users.noreply.github.com>
2023-11-22 00:09:14 -0300

    Add files via upload

--- credito.py
+++ credito.py
@@ -16,8 +16,36 @@ from xgboost import XGBClassifier
 import io
 from simulacaochurn import simulacao_churn
 
+
 def ferramenta_credito():
-    simulacao_churns = simulacao_churn()
+    min_values,max_values,model,X,scaler = simulacao_churn()
+    with st.form("prediction_form"):
+        Total_Trans_Ct = st.slider('Quantas Transações o Cliente fez nos Últimos 12 meses?',
+                                int(min_values['Total_Trans_Ct']), int(max_values['Total_Trans_Ct']))
+        Total_Trans_Amt = st.slider('Qual é o Montante Transacionado pelo Cliente nos Últimos 12 meses?',int(min_values['Total_Trans_Amt']),int(max_values['Total_Trans_Amt']))
+        Total_Revolving_Bal = st.slider('Qual é o Saldo Rotativo no Cartão de Crédito do Cliente?',int(min_values['Total_Revolving_Bal']),int(max_values['Total_Revolving_Bal']))
+        Total_Relationship_Count = st.slider('Qual é o Número Total de Produtos que o Cliente tem com o Banco?',int(min_values['Total_Relationship_Count']),int(max_values['Total_Relationship_Count']))
+        Months_Inactive_12_mon = st.slider('Quantos Meses em que o Cliente Esteve Inativo nos Últimos 12 meses?',int(min_values['Months_Inactive_12_mon']),int(max_values['Months_Inactive_12_mon']))
+        Contacts_Count_12_mon = st.slider('Quantas Vezes o Cliente Entrou em Contato com o Banco nos Últimos 12 meses?',int(min_values['Contacts_Count_12_mon']),int(max_values['Contacts_Count_12_mon']))
+
+
+        # Botão de previsão
+        if st.form_submit_button('Prever Churn'):
+            # Preparar dados para previsão
+            input_data = pd.DataFrame([[Total_Trans_Ct, Total_Revolving_Bal, Total_Relationship_Count, 
+                                        Months_Inactive_12_mon, Total_Trans_Amt, Contacts_Count_12_mon]],
+                                    columns=X.columns)
+            input_data_scaled = scaler.transform(input_data)
+            prediction = model.predict(input_data_scaled)
+            prediction_proba = model.predict_proba(input_data_scaled)[:, 1]*100
+            
+            # Mostrar resultado e probabilidade
+            if prediction == 0:
+                st.success(f'O cliente provavelmente não vai dar Churn. Probabilidade de churn: {prediction_proba[0]:.0f}%')
+            else:
+                st.error(f'O cliente provavelmente vai dar Churn. Probabilidade de churn: {prediction_proba[0]:.0f}%')
+
+    st.write("Quer descobrir como foi feita essa ferramenta? Me acompanhe nos próximos passos!")
     st.markdown('<p style="font-size:24px;">Parte 1: Primeiras impressões dos dados</p>', unsafe_allow_html=True)
     with st.echo():
         # Carregando o conjunto de dados
--- simulacaochurn.py
+++ simulacaochurn.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import MinMaxScaler
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
+@st.cache_data(show_spinner=False)
 def simulacao_churn():
     st.title("Projeto para classificar quais clientes de um banco podem dar churn")
     st.write("""
@@ -49,45 +50,39 @@ def simulacao_churn():
     model = XGBClassifier(random_state=42)
     model.fit(X_train_scaled, y_train)
 
-    # Avaliar o modelo
-    y_pred = model.predict(X_test_scaled)
-    y_pred_proba = model.predict_proba(X_test_scaled)[:, 1]
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
+    return min_values,max_values,model,X,scaler
 
-    # Streamlit UI
-    st.subheader('Ferramenta de Previsão de Churn:')
+    # # Streamlit UI
+    # st.subheader('Ferramenta de Previsão de Churn:')
 
-    with st.form("prediction_form"):
-        Total_Trans_Ct = st.slider('Quantas Transações o Cliente fez nos Últimos 12 meses?',
-                                int(min_values['Total_Trans_Ct']), int(max_values['Total_Trans_Ct']))
-        Total_Trans_Amt = st.slider('Qual é o Montante Transacionado pelo Cliente nos Últimos 12 meses?',int(min_values['Total_Trans_Amt']),int(max_values['Total_Trans_Amt']))
-        Total_Revolving_Bal = st.slider('Qual é o Saldo Rotativo no Cartão de Crédito do Cliente?',int(min_values['Total_Revolving_Bal']),int(max_values['Total_Revolving_Bal']))
-        Total_Relationship_Count = st.slider('Qual é o Número Total de Produtos que o Cliente tem com o Banco?',int(min_values['Total_Relationship_Count']),int(max_values['Total_Relationship_Count']))
-        Months_Inactive_12_mon = st.slider('Quantos Meses em que o Cliente Esteve Inativo nos Últimos 12 meses?',int(min_values['Months_Inactive_12_mon']),int(max_values['Months_Inactive_12_mon']))
-        Contacts_Count_12_mon = st.slider('Quantas Vezes o Cliente Entrou em Contato com o Banco nos Últimos 12 meses?',int(min_values['Contacts_Count_12_mon']),int(max_values['Contacts_Count_12_mon']))
+    # with st.form("prediction_form"):
+    #     Total_Trans_Ct = st.slider('Quantas Transações o Cliente fez nos Últimos 12 meses?',
+    #                             int(min_values['Total_Trans_Ct']), int(max_values['Total_Trans_Ct']))
+    #     Total_Trans_Amt = st.slider('Qual é o Montante Transacionado pelo Cliente nos Últimos 12 meses?',int(min_values['Total_Trans_Amt']),int(max_values['Total_Trans_Amt']))
+    #     Total_Revolving_Bal = st.slider('Qual é o Saldo Rotativo no Cartão de Crédito do Cliente?',int(min_values['Total_Revolving_Bal']),int(max_values['Total_Revolving_Bal']))
+    #     Total_Relationship_Count = st.slider('Qual é o Número Total de Produtos que o Cliente tem com o Banco?',int(min_values['Total_Relationship_Count']),int(max_values['Total_Relationship_Count']))
+    #     Months_Inactive_12_mon = st.slider('Quantos Meses em que o Cliente Esteve Inativo nos Últimos 12 meses?',int(min_values['Months_Inactive_12_mon']),int(max_values['Months_Inactive_12_mon']))
+    #     Contacts_Count_12_mon = st.slider('Quantas Vezes o Cliente Entrou em Contato com o Banco nos Últimos 12 meses?',int(min_values['Contacts_Count_12_mon']),int(max_values['Contacts_Count_12_mon']))
 
 
-        # Botão de previsão
-        if st.form_submit_button('Prever Churn'):
-            # Preparar dados para previsão
-            input_data = pd.DataFrame([[Total_Trans_Ct, Total_Revolving_Bal, Total_Relationship_Count, 
-                                        Months_Inactive_12_mon, Total_Trans_Amt, Contacts_Count_12_mon]],
-                                    columns=X.columns)
-            input_data_scaled = scaler.transform(input_data)
-            prediction = model.predict(input_data_scaled)
-            prediction_proba = model.predict_proba(input_data_scaled)[:, 1]*100
+    #     # Botão de previsão
+    #     if st.form_submit_button('Prever Churn'):
+    #         # Preparar dados para previsão
+    #         input_data = pd.DataFrame([[Total_Trans_Ct, Total_Revolving_Bal, Total_Relationship_Count, 
+    #                                     Months_Inactive_12_mon, Total_Trans_Amt, Contacts_Count_12_mon]],
+    #                                 columns=X.columns)
+    #         input_data_scaled = scaler.transform(input_data)
+    #         prediction = model.predict(input_data_scaled)
+    #         prediction_proba = model.predict_proba(input_data_scaled)[:, 1]*100
             
-            # Mostrar resultado e probabilidade
-            if prediction == 0:
-                st.success(f'O cliente provavelmente não vai dar Churn. Probabilidade de churn: {prediction_proba[0]:.0f}%')
-            else:
-                st.error(f'O cliente provavelmente vai dar Churn. Probabilidade de churn: {prediction_proba[0]:.0f}%')
+    #         # Mostrar resultado e probabilidade
+    #         if prediction == 0:
+    #             st.success(f'O cliente provavelmente não vai dar Churn. Probabilidade de churn: {prediction_proba[0]:.0f}%')
+    #         else:
+    #             st.error(f'O cliente provavelmente vai dar Churn. Probabilidade de churn: {prediction_proba[0]:.0f}%')
 
-    st.write("Quer descobrir como foi feita essa ferramenta? Me acompanhe nos próximos passos!")
-    # Mostrar métricas de desempenho
+    # st.write("Quer descobrir como foi feita essa ferramenta? Me acompanhe nos próximos passos!")
+    # # Mostrar métricas de desempenho
 
 
 
